420bd229b42103dd774c546f8a7c3b5fe812d0cb
achramdani <achramdani18@gmail.com>
2024-12-15 18:07:06 +0700

    punyaku

--- BengkelApp/src/Services/auth.js
+++ BengkelApp/src/Services/auth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const BASE_URL = "http://localhost:8001/api";
+const BASE_URL = "http://localhost:8000/api";
 
 export const login = async (email, password) => {
   try {
